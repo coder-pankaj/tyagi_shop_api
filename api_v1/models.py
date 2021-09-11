dae0dfed5fb6366213af8601bcbc71c681886ebe
your mobcoder-ashu <pankaj.yadav@mobcoder.com>
2021-09-11 10:51:01 +0530

    change image dir

--- api_v1/models.py
+++ api_v1/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 class Products(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='static/')
+    image = models.ImageField(upload_to='static/', blank=True, null=True)
     price = models.IntegerField()
     description = models.TextField(max_length=500, default="")
     qty = models.IntegerField()
@@ -33,7 +33,7 @@ class Products(models.Model):
 
 class SliderImages(models.Model):  
    #  category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='static/slider') 
+    image = models.ImageField(upload_to='static/slider', blank=True, null=True) 
     text_on_image =  models.CharField(max_length=256, default="", blank=True, null=True)  
     sub_text_on_image =  models.CharField(max_length=256, default="", blank=True, null=True)  
     status = models.BooleanField(default=True)
